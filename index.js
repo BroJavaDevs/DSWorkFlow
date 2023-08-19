6122e6a3d14d6bcd7ed790548934e9b6b4faf68f
trickrenzgarcia <trickrenzgarcia@gmail.com>
2023-08-19 14:01:37 +0800

    http server

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const PORT = 8080;
 
 var server = http.createServer((req, res) => {
   if (req.url == "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("./index.html");
-    res.end();
+    // Read the content of the HTML file
+    fs.readFile(path.join(__dirname, "index.html"), "utf8", (err, content) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(content);
+      res.end();
+    });
   }
 });
 
-server.listen(PORT);
+server.listen(PORT, "localhost");
